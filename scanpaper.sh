d220504cd5d05046921c78dec3857c8a72774c2c
Arild Jensen <ajensen@counter-attack.com>
2015-07-14 16:23:08 -0700

    Initial check-in

---
+++ scanpaper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://github.com/arildjensen/scripts
+# Scans pages from my flatbed scanner at about 15 kB/page.
+
+datestamp=`date -u +%s`
+
+echo ${datestamp}
+
+scanimage --mode Lineart --resolution 300 --format=tiff -b --batch-prompt --progress -l 0 -t 0 -x 215 -y 279
+tiffcp -c lzw out*.tif scan.tif
+tiff2pdf -z -o scan${datestamp}.pdf scan.tif
+rm *.tif
+ls -lh scan${datestamp}.pdf
