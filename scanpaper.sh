6f130cd452ffb454f917e439c5fc4bffcf879393
Arild Jensen <ajensen@counter-attack.com>
2019-02-01 14:35:47 -0800

    Fixed pagination

--- scanpaper.sh
+++ scanpaper.sh
@@ -7,8 +7,19 @@ datestamp=`date -u +%s`
 
 echo ${datestamp}
 
-scanimage --mode Lineart --resolution 300 --format=tiff -b --batch-prompt --progress -l 0 -t 0 -x 215 -y 279
-tiffcp -c lzw out*.tif scan.tif
+scanimage \
+  --mode Lineart \
+  --resolution 300 \
+  --format=tiff \
+  --batch="scanimage-output-%03d.tif" \
+  --batch-prompt \
+  --progress \
+  -l 0 \
+  -t 0 \
+  -x 215 \
+  -y 279
+
+tiffcp -c lzw scanimage-out*.tif scan.tif
 tiff2pdf -z -o scan${datestamp}.pdf scan.tif
 rm *.tif
 ls -lh scan${datestamp}.pdf
